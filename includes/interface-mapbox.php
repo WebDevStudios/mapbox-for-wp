d0b4f67630aa5c548ecbd8074f54717c015fa4e8
Michael Beckwith <tw2113@gmail.com>
2023-04-10 13:56:43 -0500

    start things off for an interface

---
+++ includes/interface-mapbox.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WebDevStudios\MBWP;
+
+interface Mapbox_Base {
+	public function get_endpoint();
+
+	public function set_endpoint();
+
+	public function get_version();
+
+	public function set_version();
+}
